808ee325a2a95575edd18aad1ee26f79e80c61f4
RenaissanceDesolation <musikbarrow@icloud.com>
2022-12-14 01:41:26 +0200

    Add New Repo

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/* a = 15;
+console.log(a);
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+obj.a = 10;
+console.log(obj);
+
+console.log(nameEx);
+var nameEx = 'Ivan';
+nameEx = 'Alex';
+
+{
+    var result = 50;
+}
+console.log(result);
+*/
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+console.log(something);
