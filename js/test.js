ca333c6e7bf5c897517b6dca7f44643a8f8aaa04
RenaissanceDesolation <musikbarrow@icloud.com>
2023-01-16 02:06:15 +0200

    homework

--- js/test.js
+++ js/test.js
@@ -89,7 +89,8 @@ for (let i = 0; i <= lines; i++) {
 console.log(result)
 */
 
-/*let num = 20;
+/*
+let num = 20;
 
 function showFirstMessage(text) {
     console.log(text);
@@ -113,7 +114,8 @@ switch (x) {
 }
 */
 
-/* const usdCurr = 28;
+/*
+const usdCurr = 28;
 const discount = 0.9;
 function convert(amount, curr) {
     return curr * amount;
@@ -132,5 +134,111 @@ function test() {
     console.log('Done');
 }
 test();
+*/
+
+/*
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(plan) {
+        const {age} = plan;
+        const {languages} = plan.skills;
+        let str = `Мне ${age} и я владею языками: `;
+
+        languages.forEach(function(lang) {
+            str += `${lang.toUpperCase()} `;
+        });
+
+        return str;
+    }
+};
+
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
+
+function showExperience(plan) {
+    const {exp} = plan.skills;
+    return exp;
+}
+
+showExperience(personalPlanPeter);
+
+function showProgrammingLangs(plan) {
+    let str = '';
+    const {programmingLangs} = plan.skills;
+    for (let key in programmingLangs) {
+        str += `Язык ${key} изучен на ${programmingLangs[key]}\n`
+    }
+
+    return str;
+}
+
+showProgrammingLangs(personalPlanPeter);
+*/
+
+/*
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    let str = '';
+
+    arr.length === 0 ? str = 'Семья пуста' : str = 'Семья состоит из: ';
+
+    arr.forEach(member => {
+        str += `${member} `
+    });
+
+    return str;
+}
+
+showFamily(family);
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    arr.forEach(city => {
+        console.log(city.toLowerCase())
+    })
+}
+
+standardizeStrings(favoriteCities);
+*/
+
+/*
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    if (typeof(str) !== 'string') {
+        return "Ошибка!";
+    }
+        return str.split('').reverse().join('');
+    }
+
+reverse(someString);
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+    let str = '';
+    arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+      for (let i = 0; i < arr.length; i++) {
+         if (arr[i] === missingCurr) {
+             continue;
+         }
+         str += `${arr[i]}\n`;
+     }
+
+    return str;
+}
+
+availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY')
 */
 
